22a363a83e5e42d03448f83e95882204ff4d10de
Vivek Kumar <72351936+8521324089@users.noreply.github.com>
2025-05-30 00:14:19 +0530

    Add files via upload

---
+++ src/main/java/com/vk_mail/Email_Sender/service/SendEmailService.java
@@ -0,0 +1,28 @@
+package com.vk_mail.Email_Sender.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SendEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender ;
+
+    //@Value("$(spring.mail.username)")
+    private String fromEmailId = "vk27092002@gmail.com";
+
+    public void sendEmail(String recipient,String body,String subject){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom(fromEmailId);
+        simpleMailMessage.setTo(recipient);
+        simpleMailMessage.setText(body);
+        simpleMailMessage.setSubject(subject);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+
+}
---
+++ src/test/java/com/vk_mail/Email_Sender/EmailSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk_mail.Email_Sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
